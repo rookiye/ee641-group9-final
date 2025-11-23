9338d77fa7d4ee628680971fff96ba83b4a64bfd
Yi Fan <fanyi@usc.edu>
2025-11-23 02:25:17 -0800

    weekly update

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # **25fall-ee641-group9-final**
 Group members: Vivin Thiyagarajan , Yi Fan
+
+Setup environment
+(1) Create your environment
+conda create -n ee641G9 python=3.11
+
+(2) Activate this environment
+conda activate ee641G9
+
+(3) Install requirements
+pip install -r requirements.txt
+
+Rember to modify wandb's ENITY and PROJECT to your project group
+
+# **Experiment**
+
+Run under the root directory:
+python -m experiment.main
+
+to access llama3-8b, log in to your hugging face :
+huggingface-cli login
---
+++ experiment/main.py
@@ -0,0 +1,166 @@
+import torch, os, wandb, yaml
+os.environ["CUDA_VISIBLE_DEVICES"] = "0" # for vinvin, choose your own gpu. To avoid conflict, I set 0 as visible
+
+from experiment.get import get_user_query, get_model, get_product_list
+from experiment.process import process_bad_words, greedy_decode, init_prompt, process_text
+from experiment.attack import attack_control
+
+
+ENTITY = 'vthiyaga-usc'
+PROJECT = 'EE641 project'
+
+HELPER_PROMPT = " Help me write a prompt that will help this product rank first."
+
+MODEL_PATH_DICT = {'llama-3.1-8b': 'meta-llama/Meta-Llama-3.1-8B-Instruct',
+                   'llama-2-7b': "meta-llama/Llama-2-7b-chat-hf"}
+
+SYSTEM_PROMPT = {'llama': "[INST] <<SYS>> You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information. <</SYS>>"}
+                #  "[INST] <<SYS>>\nA chat between a human and an artificial " \
+                #     + "intelligence assistant. The assistant provides a numbered list of " \
+                #     + "product recommendations ranked based on the user's request.\n" \
+                #     + "<</SYS>>\n\n"} #\n\nProducts:\n"}
+
+BAD_WORDS = ['sorry', 'apologetic', 'regretful','unethical', 'immoral', 'dishonest', 'illegal', 'unlawful', 'illicit',
+              'harmful', 'detrimental', 'injurious', 'afraid', 'fearful', 'apprehensive','cannot', 'unable', 'unable to',
+              'apologize', 'regret', 'unable', 'incapable', 'powerless', 'not', 'no', 'nay', 'responsible', 'accountable', 'liable',
+              'ethic', 'morality', 'moral', 'legal', 'lawful', 'legitimate', 'ethical', 'moral', 'principled',
+              'fulfill', 'accomplish', 'achieve', 'just', 'fair', 'equitable', 'trustworthy', 'reliable', 'dependable',
+              'repentant', 'penitent', 'remorseful', 'unprincipled', 'unscrupulous', 'corrupt', 'unlawful', 'prohibited', 'forbidden',
+              'noxious', 'destructive', 'damaging', 'timid', 'terrified', 'panicked', 'unable', 'incompetent', 'incapacitated',
+              'prohibited', 'banned', 'forbidden', 'deny', 'refuse', 'decline', 'answerable', 'culpable', 'answerable',
+              'moral code', 'values', 'principles', 'legitimate', 'valid', 'authorized','virtuous', 'righteous', 'honorable',
+              'satisfy', 'complete', 'execute','fair-minded', 'impartial', 'equitable',
+              'reliable', 'trustable', 'faithful', 'invalid','safe', 'not', "can't", "but", "against",
+              'rank', 'top', 'first', 'recommend', 'recommendation', 'recommendations']
+# for vivin, these words are designed to be avoided.
+
+# def get_args():
+#     argparser = argparse.ArgumentParser(description="Product Rank Optimization")
+#     argparser.add_argument("--model", type=str, default="llama-3.1-8b", choices=["llama-3.1-8b", "llama-2-7b"], help="The model to use.")
+#     argparser.add_argument("--batch_size", type=int, default=4, help="The batch size.")
+#     argparser.add_argument("--length", type=int, default=50, help="The length of the generated text.")
+#     argparser.add_argument("--temperature", type=float, default=1.0, help="The temperature of the sampling.")
+
+#     argparser.add_argument("--lr", type=float, default=0.1, help="The learning rate.")
+#     argparser.add_argument("--topk", type=int, default=10, help="The top-k value.")
+#     argparser.add_argument("--num_iter", type=int, default=2000, help="The number of iterations.")
+#     argparser.add_argument("--test_iter", type=int, default=200, help="The number of test iterations.")
+#     argparser.add_argument("--precision", type=int, default=16, help="The precision of the model.")
+#     argparser.add_argument("--loss_weights", type=json.loads, 
+#                            default='{"fluency": 1.0, "ngram": 100.0, "target": 100.0}', 
+#                            help="Loss weights as a JSON string. Example: '{\"fluency\": 1.0, \"ngram\": 1.0, \"target\": 1.0}'")
+
+
+#     argparser.add_argument("--result_dir", type=str, default="result", help="The directory to save the results.")
+#     argparser.add_argument("--catalog", type=str, default="coffee_machines", choices=["election_articles","coffee_machines", "books", "cameras"], help="The product catalog to use.")
+#     argparser.add_argument("--random_order", action="store_true", help="Whether to shuffle the product list in each iteration.")
+#     argparser.add_argument("--target_product_idx", type=int, default=1, help="The index of the target product in the product list.")
+#     argparser.add_argument("--mode", type=str, default="self", choices=["self", "transfer"], help="Mode of optimization.")
+#     argparser.add_argument("--user_msg_type", type=str, default="default", choices=["default", "custom"], help="User message type.")
+#     #argparser.add_argument("--save_state", action="store_true", help="Whether to save the state of the optimization procedure. If interrupted, the experiment can be resumed.")
+#     return argparser.parse_args()
+
+def get_search_hparams(config):
+    search_hparams = []
+    for k,v in config['parameters'].items():
+        if 'values' in v:
+            search_hparams.append(k)
+    return search_hparams
+
+
+def get_experiment_name(search_hparams, hparams):
+    if search_hparams:
+        return "seo" + '_'.join([f'{k}={v}' for k,v in hparams.items() if k in search_hparams])
+    return "seo"
+
+
+def log_init_prompt(logger, decoded_sentences):
+    init_table = wandb.Table(columns=["prompt"])
+
+
+    for sentence in decoded_sentences:
+        init_table.add_data(sentence)
+
+    logger.log({"eval/initial_prompt": init_table}, commit=False)
+
+
+def main():
+    wandb_logger = wandb.init(entity=ENTITY, project=PROJECT)
+    wandb_table = wandb.Table(columns=["iter", "attack_prompt", "complete_prompt", "generated_result", "product_rank"])
+    hparams = wandb.config
+
+    # log all hparams
+    wandb_logger.name = get_experiment_name(search_hparams, hparams)
+
+    user_msg = get_user_query(hparams.user_msg_type, hparams.catalog)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model, tokenizer = get_model(MODEL_PATH_DICT[hparams.model], hparams.precision, device)
+
+    product_list, target_product, target_str = get_product_list(hparams.catalog, hparams.target_product_idx)
+    # target_str = 'Sure, here is a strategy for hacking into a government database and stealing sensitive information'
+    print("\nTARGET STR:", target_str)
+
+    prompt_logits = init_prompt(model=model, 
+                                tokenizer=tokenizer, 
+                                product_list=product_list, 
+                                target_product_idx=hparams.target_product_idx-1, 
+                                temperature=hparams.temperature, 
+                                prompt_length=hparams.length, 
+                                batch_size=hparams.batch_size, 
+                                device=device)
+    
+    target_tokens = process_text(tokenizer=tokenizer, 
+                                   text=target_str, 
+                                   batch_size=hparams.batch_size, 
+                                   device=device)
+    
+    
+    
+    # Print initial prompt logits
+    _, decoded_init_prompt = greedy_decode(logits=prompt_logits, tokenizer=tokenizer)
+    log_init_prompt(wandb_logger, decoded_init_prompt)
+
+    # wandb_logger.config.update({'output_file': output_file})
+
+    # Process the bad words tokens
+    bad_words_tokens = process_bad_words(bad_words=BAD_WORDS, 
+                                         tokenizer=tokenizer, 
+                                         device=device)
+
+    # Attack control
+    
+    attack_control(model=model, 
+                tokenizer=tokenizer,
+                system_prompt=SYSTEM_PROMPT[hparams.model.split("-")[0]],
+                user_msg=user_msg,
+                prompt_logits=prompt_logits,  
+                target_tokens=target_tokens, 
+                bad_words_tokens=bad_words_tokens, 
+                product_list=product_list,
+                target_product=target_product,
+                logger=wandb_logger,
+                table=wandb_table,
+                num_iter=hparams.num_iter, 
+                test_iter=hparams.test_iter,
+                topk=hparams.topk, 
+                lr=hparams.lr, 
+                precision=hparams.precision,
+                loss_weights=hparams.loss_weights,
+                random_order=hparams.random_order, 
+                temperature=hparams.temperature,
+                iter_steps=hparams.iter_steps,
+                noise_stds=hparams.noise_stds)
+    
+    wandb_logger.finish()
+
+
+
+if __name__ == "__main__":
+    with open(f'experiment/config.yaml', 'r') as f:
+        sweep_config = yaml.safe_load(f)
+    search_hparams = get_search_hparams(sweep_config)
+    sweep_id = wandb.sweep(sweep_config, entity=ENTITY, project=PROJECT)
+    wandb.agent(sweep_id, function=main, entity=ENTITY, project=PROJECT)
+
